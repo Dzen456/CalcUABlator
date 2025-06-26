4d779813d902acabf7f7a92e4a1d3124b448452f
David Zheng <155198401+Dzen456@users.noreply.github.com>
2025-06-26 12:19:08 +0200

    Update connect_arduino.py

--- Software_CalcUABlator/RaspberryPi/connect_arduino.py
+++ Software_CalcUABlator/RaspberryPi/connect_arduino.py
@@ -1,7 +1,7 @@
 import serial
 
-def send_result_to_arduino(value1, value2, port='/dev/ttyACM0', baud_rate=9600):
-    data = f"{value1},{value2}"
+def send_result_to_arduino(motor1, motor2, result, port='/dev/ttyACM0', baud_rate=9600):
+    data = f"{motor1},{motor2},{result}\ "
     with serial.Serial(port, baud_rate, timeout=1) as ser:
         ser.write(data.encode())
         print(f"Sent to Arduino: {data}")

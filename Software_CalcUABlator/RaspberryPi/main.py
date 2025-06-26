c58cb79ff0951f899127fc07bce499a2ddf4c622
David Zheng <155198401+Dzen456@users.noreply.github.com>
2025-06-26 19:25:32 +0200

    Update main.py

--- Software_CalcUABlator/RaspberryPi/main.py
+++ Software_CalcUABlator/RaspberryPi/main.py
@@ -5,17 +5,17 @@ from connect_arduino import *
 #Text Recognition
 preprocess_image("test2.jpg", "processed_image.jpg")
 expression, start_coordinates=extract_text("processed_image.jpg")
-print(expression)
-print(start_coordinates)
+print("Expressio identificada: ", expression)
+print("Coordenades imatge: ", start_coordinates)
 filtered_expression=filter_expression(expression)
-print(filtered_expression)
+print("Expressio filtrada: ", filtered_expression)
 answer=resolve_math_expression(filtered_expression)
-print(answer)
+print("Solucio calculada: "answer)
 
 #Robot Movement Calculations
 x_robot, y_robot = transform_coordinates(start_coordinates[0])
-print(x_robot, y_robot)
+print("Coordenades robot: ", x_robot, y_robot)
 q = calculate_angles(x_robot, y_robot)
-print(q)
+print("Angles calculats: ", q)
 
 send_result_to_arduino(q[0], q[1], answer)
